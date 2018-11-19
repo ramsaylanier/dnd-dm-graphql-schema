0831946133f2a624ce30f139a1f6bb704a7c9d72
Ramsay Lanier <ramsaylanier@gmail.com>
2018-11-18 19:07:18 -0500

    started encounter resolver

--- src/resolvers/encounter.js
+++ src/resolvers/encounter.js
@@ -8,10 +8,43 @@ const determineXpBudget = (playerLevels, difficulty) => {
   return threshold
 }
 
-const generateEncounter = ({ playerLevels, difficulty }) => {
+const determineXPMultiplier = n => {
+  if (n === 1) {
+    return 1
+  } else if (n === 2) {
+    return 1.5
+  } else if (n >= 3 && n <= 6) {
+    return 2
+  } else if (n >= 7 && n <= 10) {
+    return 2.5
+  } else if (n >= 11 && n <= 14) {
+    return 3
+  } else {
+    return 4
+  }
+}
+
+const generateMonstersForEncounter = ({
+  xpThreshold,
+  environment,
+  maxMonsters
+}) => {}
+
+const generateEncounter = ({
+  playerLevels,
+  difficulty,
+  environment,
+  maxMonsters
+}) => {
   const xpThreshold = determineXpBudget(playerLevels, difficulty)
+  const monsters = generateMonstersForEncounter({
+    xpThreshold,
+    environment,
+    maxMonsters
+  })
   return {
-    xpThreshold
+    xpThreshold,
+    monsters
   }
 }
 
